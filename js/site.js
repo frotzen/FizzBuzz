1a21fcef98e6908dd012e93d02c8328c315de449
Elwin Beall <frotzen@gmail.com>
2022-10-11 15:53:26 -0500

    commit with improvements

--- js/site.js
+++ js/site.js
@@ -5,12 +5,13 @@
 function getValues() {
   let firstFactor = document.getElementById("firstFactor").value;
   let secondFactor = document.getElementById("secondFactor").value;
+  let stopValue = document.getElementById("stopValue").value;
 
   firstFactor = parseInt(firstFactor);
   secondFactor = parseInt(secondFactor);
 
   if (Number.isInteger(firstFactor) && Number.isInteger(secondFactor)) {
-    let numbers = generateFizzBuzz(firstFactor, secondFactor);
+    let numbers = generateFizzBuzz(firstFactor, secondFactor, stopValue);
     displayFizzBuzz(numbers);
   } else {
     Swal.fire({
@@ -21,9 +22,9 @@ function getValues() {
   }
 }
 
-function generateFizzBuzz(first, second) {
+function generateFizzBuzz(first, second, stop) {
   let alNumArray = [];
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= stop; i++) {
     if (i % first == 0 && i % second == 0) {
       alNumArray.push("FizzBuzz");
     } else if (i % first == 0) {
@@ -37,13 +38,40 @@ function generateFizzBuzz(first, second) {
   return alNumArray;
 }
 
+// function displayFizzBuzz(items) {
+//   let length = items.length;
+//   let templateRows = "";
+//   for (let i = 0; i < length; i++) {
+//     let number = items[i];
+//     templateRows += `<tr><td>${number}</td></tr>`;
+//   }
+//   document.getElementById("results").innerHTML = templateRows;
+// }
+
 function displayFizzBuzz(items) {
   let length = items.length;
-  let className = "even";
+  let className = "nonFactor";
   let templateRows = "";
   for (let i = 0; i < length; i++) {
     let number = items[i];
-    templateRows += `<tr><td>${number}</td></tr>`;
+
+    if (number == "Fizz") {
+      className = "bg-warning";
+    } else if (number == "Buzz") {
+      className = "bg-dark text-light";
+    } else if (number == "FizzBuzz") {
+      className = "bg-danger text-warning";
+    } else className = "table-";
+
+    if (i % 5 == 0) {
+      templateRows += "<tr>";
+    }
+    templateRows += `<td class="${className}"> ${number} </td>`;
+
+    if ((i + 1) % 10 == 0) {
+      templateRows += "</tr>";
+    }
   }
+
   document.getElementById("results").innerHTML = templateRows;
 }
